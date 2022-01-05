b91e19d9221496679f94718534a4c5d72a541273
92SooJong <hsz3855@gmail.com>
2022-01-06 00:36:16 +0900

    게시글 수정 및 삭제 기능 추가

--- src/main/java/com/soojong/springboot/web/dto/PostsUpdateRequestDto.java
+++ src/main/java/com/soojong/springboot/web/dto/PostsUpdateRequestDto.java
@@ -3,15 +3,18 @@ package com.soojong.springboot.web.dto;
 import com.soojong.springboot.domain.posts.Posts;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class PostsUpdateRequestDto {
 
     private String title;
     private String content;
 
-    @Builder
 
+
+    @Builder
     public PostsUpdateRequestDto(String title, String content) {
         this.title = title;
         this.content = content;
