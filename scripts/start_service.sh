bfecb16867f20aba92cd10aec038e4432efa8fd6
92SooJong <hsz3855@gmail.com>
2022-01-20 21:19:05 +0900

    실행 스크립트 변경

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -2,4 +2,4 @@
 
 echo "> 구동"
 
-nohup java -jar /home/ec2-user/app/step2/spring-boot-aws/build/libs/spring-boot-aws-1.0-SNAPSHOT.jar > nohup.out 2>&1
+java -jar /home/ec2-user/app/step2/build/libs/spring-boot-aws-1.0-SNAPSHOT.jar
